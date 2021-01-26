fb9512dbd90dcbdcc4a955fe4b6908d231d26c25
unknown <maktabisaid1@gmail.com>
2021-01-26 17:09:45 +0100

    + exo while et do..while

---
+++ exo_boucle_while/main.js
@@ -0,0 +1,43 @@
+// EXO 1
+
+// let chiffre = prompt(`Choisissez un chiffre :`);
+// let compteur;
+
+
+// if (chiffre>0) {
+//     compteur = 0;
+//     while(compteur<=chiffre){
+//         console.log(compteur);
+//         compteur++;
+//     }
+// } else {
+//     compteur = 0;
+//     while(compteur>=chiffre){
+//         console.log(compteur);
+//         compteur--;
+//     }
+// }
+
+// EXO 2
+
+// let classe = ["Mohammed", "Yasmina", "Amine", "Salim", "Abdel", "Majda", "Bilal", "Laurie", "Yahya", "Mahad", "Nghia", "Saïd", "Camille", "Antoine", "Ayoub", "Yassin", "Cem", "Gauthier"];
+// let i = 0;
+// while(i < classe.length){
+//     console.log(`Bonjour `+ classe[i]);
+//     i++;
+// }
+
+// EXO 3
+
+// let classe = [];
+// let maxStudent;
+
+// do {
+//     maxStudent = parseInt(prompt(`Entrez un nombre max d'étudiants : `));
+// } while (isNaN(maxStudent));
+
+// while(classe.length < maxStudent){
+//     classe.push(prompt(`Ajouter un élève à la classe : `));
+// }
+
+// console.table(classe);
---
+++ exo_doWhile/main.js
@@ -0,0 +1,35 @@
+// EXO 1
+
+// let chiffre = parseInt(prompt(`Choisissez un chiffre : `));
+// let compteur = 0;
+
+// if (chiffre>0) {
+//     do {
+//         console.log(compteur);
+//         compteur++;
+//     } while (compteur<=chiffre);
+// } else {
+//     do {
+//         console.log(compteur);
+//         compteur--;
+//     } while (compteur>=chiffre);
+// }
+
+// EXO 2
+
+// let classe = ["Mohammed", "Yasmina", "Amine", "Salim", "Abdel", "Majda", "Bilal", "Laurie", "Yahya", "Mahad", "Nghia", "Saïd", "Camille", "Antoine", "Ayoub", "Yassin", "Cem", "Gauthier"];
+// let i = 0;
+// do {
+//     console.log(`Bonjour ` + classe[i]);
+//     i++;
+// } while (i<classe.length);
+
+// EXO 3
+
+// let fruits = ["fraise","banane","poire","framboise","pomme","avocat"];
+// console.log(fruits);
+// do {
+//     fruits.shift();
+// } while (fruits.length>0);
+
+// console.log(fruits);
